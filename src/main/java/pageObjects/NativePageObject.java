c571228daa98e31d39b0974cbcab98abd8015272
VictoriaBobrikova <bobtory@yandex.ru>
2021-02-20 10:06:23 +0300

    ex3 ios locators native

--- src/main/java/pageObjects/NativePageObject.java
+++ src/main/java/pageObjects/NativePageObject.java
@@ -3,33 +3,46 @@ package pageObjects;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.appium.java_client.pagefactory.iOSXCUITFindBy;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 public class NativePageObject  {
 
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeTextField[@value='user@example.com']")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/login_email")
     WebElement emailInputField;
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeSecureTextField[@value='Required']")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/login_pwd")
     WebElement passwordInputField;
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeStaticText[@label='Sign In']")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/email_sign_in_button")
     WebElement signInButton;
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeStaticText[@value='Register new account']")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/register_button")
     WebElement registerButton;
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeTextField[@value='user@example.com']")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_email")
     WebElement registrationEmailInputField;
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeTextField[@value='TimApple']")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_username")
     WebElement registrationUsernameInputField;
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeSecureTextField[@value='Required']")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_password")
     WebElement registrationPasswordInputField;
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeSecureTextField[@value='Repeat please']")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_confirm_password")
     WebElement confirmPasswordInputField;
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeStaticText[@label='Register new account']")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/register_new_account_button")
     WebElement registerNewAccountButton;
 //    @AndroidFindBy(xpath = "//*[contains(@text, 'BudgetActivity')]")
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeStaticText[@value='BudgetActivity']")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/action_bar")
 //    @AndroidFindBy(className = "android.widget.TextView")
     WebElement budgetActivity;
+
+    //switch XCUIElementTypeSwitch
 
     public NativePageObject(AppiumDriver appiumDriver) {
         PageFactory.initElements( new AppiumFieldDecorator(appiumDriver), this);
